97d6027f6588e0908a17f831120155118d2aa3ab
Sandro Padin <sandropadin@gmail.com>
2014-03-21 20:21:40 -0500

    adding description for compose task

--- lib/gmail_tool.rb
+++ lib/gmail_tool.rb
@@ -5,6 +5,7 @@ class GmailToolRunner < Boson::Runner
   option :to,      type: :string
   option :subject, type: :string
   option :body,    type: :string
+  desc "Open a pre-filled compose message"
   def compose(options)
     @options = options
     open_compose_url
